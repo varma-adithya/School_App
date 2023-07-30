b6c395244695271969c0e0e8a1e1ab3eb972220e
Adithya Varma <bahadithya@gmail.com>
2023-07-30 21:02:04 +0530

    Clean code object Student

---
+++ Models/Subject.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace School_App.Models
+{
+    public class Subject
+    {
+        public int SubjectsId { get; set; }
+        public string SubjectName { get; set; }
+        public string? Description { get; set; }
+    }
+}
