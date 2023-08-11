c29e274da83e30b99470ca3abb0ab70343096d1e
Abhilash <abhilashgraja@gmail.com>
2023-08-11 07:29:37 +0530

    added AcademicYearService and created relations between Data,Business,Front

---
+++ School.Business/AcademicYearService.cs
@@ -0,0 +1,24 @@
+﻿using School.Data.Models;
+using School.Data.Repository;
+
+namespace School.Business
+{
+    public class AcademicYearService
+    {
+        private readonly IRepository<AcademicYear> _repository;
+
+        public AcademicYearService(IRepository<AcademicYear> repository)
+        {
+            _repository = repository;
+        }
+
+        public void AddAcademicYear(AcademicYear academicYear) 
+            => _repository.Add(academicYear);
+
+        public void UpdateAcademicYear(AcademicYear academicYear)
+            => _repository.Update(academicYear);
+
+        public AcademicYear GetAcademicYear(int id)
+            => _repository.GetById(id);
+    }
+}
--- School.Console/Program.cs
+++ School.Console/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using School.Business;
+using School.Data.Models;
+using School.Data.Repository;
+
 Console.WriteLine("Hello, World!");
+
+// CRUD Academic Year
+var academicYearService = new AcademicYearService(new Repository<AcademicYear>(null));
+academicYearService.AddAcademicYear(new AcademicYear { StartYear = 2023, EndYear = 2024 });
